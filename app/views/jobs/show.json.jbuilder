29b696c09efc62fc224f751e71c469e5a176fc5b
Chris Scott <cyberstrike@gmail.com>
2016-05-28 21:12:34 -0400

    scaffolded jobs

---
+++ app/views/jobs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @job, :id, :title, :link, :location, :description, :remote, :exp_date, :created_at, :updated_at
---
+++ spec/factories/jobs.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :job do
+    title "MyString"
+    link "MyString"
+    location "MyString"
+    description "MyText"
+    remote ""
+    exp_date "2016-05-28"
+  end
+end
