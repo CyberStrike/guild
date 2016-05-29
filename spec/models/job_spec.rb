c79330f058702a935fde8d62452a964281decdf8
Brian Kim <bkim8815@gmail.com>
2016-05-29 00:31:07 -0400

    RSPEC added for the model

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Job, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#instantiate" do
+    it "should generate new instance" do
+      newclass=described_class.new
+      expect(newclass).to be_a(described_class)
+    end
+  end
+  describe "#archive" do
+    let(:job){ Job.new }
+    it "should set the default archived boolean to false" do
+
+      expect( job.archived ).to eq false
+    end
+  end
 end
